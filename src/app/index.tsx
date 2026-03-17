1fc470224321391ca8af8d5750f6c646da584020
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2026-03-17 10:08:53 +0500

    install and setup native wind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Text, View } from "react-native";
 
-export default function Index() {
+const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold">Home</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default Home;
